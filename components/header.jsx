f30a2e3b5e2929503ba8df0c41305c6e418278e5
Vansh <vanshrajotia0266@gmail.com>
2026-07-03 07:46:43 +0530

    Setup User Authorization

--- components/header.jsx
+++ components/header.jsx
@@ -1,9 +1,19 @@
+import {Show, SignInButton,SignUpButton, UserButton } from '@clerk/nextjs'
 import React from 'react'
 
 const Header = () => {
   return (
     <div>
-      Header
+      {/* Replaces <SignedOut> */}
+      <Show when="signed-out">
+        <SignInButton />
+        <SignUpButton />
+      </Show>
+      
+      {/* Replaces <SignedIn> */}
+      <Show when="signed-in">
+        <UserButton />
+      </Show>
     </div>
   )
 }
